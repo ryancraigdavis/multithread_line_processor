a825e3fa06250fc663c8a4fbd28fc371270c34a4
Ryan Davis <ryancraigdavis@gmail.com>
2020-08-13 20:39:25 -0700

    changes

--- line_processor.c
+++ line_processor.c
@@ -52,35 +52,11 @@ pthread_cond_t buffer2_empty = PTHREAD_COND_INITIALIZER;
 pthread_cond_t buffer3_full = PTHREAD_COND_INITIALIZER;
 pthread_cond_t buffer3_empty = PTHREAD_COND_INITIALIZER;
 
-// This function reads in from stdin
-const char* read_input() {
-
-    // Variable for the input
-    char *line = NULL;
-    line = (char *)malloc(1000*sizeof(char));
-    memset((char*) line, '\0', sizeof(*line));
-
-    // Read in the users input
-    fgets(line, 1000, stdin);
-  
-    return line;
+void prints(char* s){
+    printf("%s\n",s);
 }
 
-// This function exchanges a "\n" for a space
-const char* line_separator(char *line) {
-
-	// Declare the return string
-	char *return_sep_line = NULL;
-    return_sep_line = (char *)malloc((1100)*sizeof(char));
-    memset(return_sep_line, '\0', 1100);
-
-    // Copy from paramter and append " "
-    strcpy(return_sep_line, line);
-    strcat(return_sep_line, " ");
-      	
-  	// Return new line with space
-	return return_sep_line;
-}
+int all_done = 0;
 
 // This function exchanges a "++" for a "^"
 // This code is modified from my Smallsh.c program where I replace "$$" with PID
@@ -155,65 +131,6 @@ const char* plus_sign(char *line) {
     return carrot_line;
 }
 
-// This function prints to stdout
-const char* write_output(char *line) {
-
-    // Declare the return string which will hold the reamining chars not printed
-    char *return_str = NULL;
-    return_str = (char *)malloc((1100)*sizeof(char));
-    memset(return_str, '\0', 1100);
-
-    // Temp string 1, copy from buffer
-    char *temp_str = NULL;
-    temp_str = (char *)malloc((1100)*sizeof(char));
-    memset(temp_str, '\0', 1100);
-    strcpy(temp_str, line);
-
-    // Temp string 2, keep blank for now
-    char *temp_str2 = NULL;
-    temp_str2 = (char *)malloc((1100)*sizeof(char));
-    memset(temp_str2, '\0', 1100);
-
-    // Print string is for printing to output if conditions are satisfied
-    char *print_str = NULL;
-    print_str = (char *)malloc((1100)*sizeof(char));
-    memset(print_str, '\0', 1100);
-
-    // Start by declaring break_loop to false, this assumes that a string is too short
-    bool break_loop = false;
-
-    // First check to see if the string pass through buffer is DONE\n
-    // if it is, skip to the end
-    int output_first_bool_cmp = strcmp(return_str,"DONE\n");
-    if (output_first_bool_cmp != 0) {
-
-        // Loop through the buffer string until less than 80 chars are found
-        // We will loop as many times as it takes to get to less than 80
-        while(break_loop == false){
-
-            // If less than 80 chars found, pass the result to the next buffer and break
-            // out of the loop
-            if (strlen(temp_str) < 80) {
-                strcpy(return_str, temp_str);
-                break_loop = true;
-
-            // Else print the string
-            } else {
-                strncpy(print_str, temp_str, 80);
-                fprintf(stdout, "%s",print_str);
-                fflush(stdout);
-
-                // This passes the chars after 80 back to temp_str
-                // We must do this in case more than 160 chars are found
-                strcpy(temp_str2, &temp_str[80]);
-                memset(temp_str, '\0', 1100);
-                strcpy(temp_str, temp_str2);
-            }
-        }
-    }
-    return return_str; 
-}
-
 // Input producer thread - calls the read_input function
 void *b1_producer(void *args) {
     // Buffer is not DONE boolean - keeps the loop going until DONE\n is found
@@ -222,18 +139,27 @@ void *b1_producer(void *args) {
     // Lock the mutex before checking where there is space in the buffer
     while (input_bool == true) {
 
-        // Input line is for checking for DONE\n
-        char *input_line = NULL;
-        input_line = (char *)malloc((1100)*sizeof(char));
-        memset(input_line, '\0', 1100);
 
         // Call read method and copy the result to input_line
-        strcpy(input_line, read_input());
+        // Variable for the input
+        char *line = NULL;
+        line = (char *)malloc(1000*sizeof(char));
+        memset((char*) line, '\0', sizeof(*line));
+
+        char* f_line;
+        // Read in the users input
+        f_line = fgets(line, 1000, stdin);
+
+        if (f_line == NULL) {
+            input_bool = false;
+            all_done = 1;
+        }
 
         // Checks to see if only DONE\n was read
-        int input_bool_cmp = strcmp(input_line,"DONE\n");
+        int input_bool_cmp = strcmp(line,"DONE\n");
         if (input_bool_cmp == 0) {
             input_bool = false;
+            all_done = 1;
         }
 
         pthread_mutex_lock(&b1_mutex);
@@ -242,7 +168,7 @@ void *b1_producer(void *args) {
         while(buffer1_count == 1)
             pthread_cond_wait(&buffer1_empty, &b1_mutex);
         
-        buffer1[buffer1_pro_idx] = strdup(input_line);
+        buffer1[buffer1_pro_idx] = strdup(line);
     
         buffer1_pro_idx = (buffer1_pro_idx + 1) % B1_SIZE;
         buffer1_count++;
@@ -271,7 +197,7 @@ void *b1_cons_b2_pro(void *args) {
 		// Buffer is empty. Wait for the producer to signal that the buffer has data
 	    while (buffer1_count == 0)
 	    	pthread_cond_wait(&buffer1_full, &b1_mutex);
-		
+
 		// Line Sep line for copying the buffer
 		char *endsep_line = NULL;
     	endsep_line = (char *)malloc((1100)*sizeof(char));
@@ -287,19 +213,12 @@ void *b1_cons_b2_pro(void *args) {
         char *spacesep_line = NULL;
         spacesep_line = (char *)malloc((1100)*sizeof(char));
         memset(spacesep_line, '\0', 1100);
+        strcpy(spacesep_line, buffer1[buffer1_con_idx]);
 
-        // If DONE\n was passed, don't remove \n from the line
-        if (linesep_bool == true) {
-
-            // Copy the buffer over but -1 character (carriage return)
-            strncpy(endsep_line, buffer1[buffer1_con_idx], strlen(buffer1[buffer1_con_idx])-1);
-
-            // Call linesep method and copy the result to input_line
-            strcpy(spacesep_line, line_separator(endsep_line));
-        } else {
-
-            // Input was DONE\n - do not remove the carriage return, skip the line sep function
-            strcpy(spacesep_line, buffer1[buffer1_con_idx]);
+        for (int i = 0; i < strlen(spacesep_line); ++i) {
+            if (spacesep_line[i] == '\n'){
+                spacesep_line[i] = ' ';
+            }
         }
         
         // Increment the consumer buffer count and decrement the main buffer count
@@ -349,7 +268,7 @@ void *b2_cons_b3_pro(void *args) {
         // Buffer is empty. Wait for the producer to signal that the buffer has data
         while (buffer2_count == 0)
             pthread_cond_wait(&buffer2_full, &b2_mutex);
-        
+
         // Plus sign line for copying the buffer
         char *plus_sign_line = NULL;
         plus_sign_line = (char *)malloc((1100)*sizeof(char));
@@ -362,6 +281,7 @@ void *b2_cons_b3_pro(void *args) {
         int plus_sign_bool_cmp = strcmp(plus_sign_line,"DONE\n");
         if (plus_sign_bool_cmp == 0) {
             plus_bool = false;
+
         }
 
         // Increment the consumer buffer count and decrement the main buffer count
@@ -411,8 +331,10 @@ void *b3_consumer(void *args) {
 
     // Output line - used for printing, checking for done, and holding extra chars
     char *output_line = NULL;
-    output_line = (char *)malloc((1100)*sizeof(char));
-    memset(output_line, '\0', 1100);
+    output_line = (char *)malloc((10000)*sizeof(char));
+    memset(output_line, '\0', 10000);
+
+    int total_lines = 0;
 
     // Buffer is not DONE boolean - keeps the loop going until DONE\n is found
 	bool output_bool = true;
@@ -427,23 +349,36 @@ void *b3_consumer(void *args) {
 	    while (buffer3_count == 0)
 	    	pthread_cond_wait(&buffer3_full, &b3_mutex);
 
+        // Checks to see if only DONE\n was written
+        int output_bool_cmp = strcmp(buffer3[buffer3_con_idx],"DONE\n");
+        if (output_bool_cmp == 0) {
+            output_bool = false;
+        }
+
         // Intermediate Output line - copy for buffer
         char *temp_output_line = NULL;
         temp_output_line = (char *)malloc((1100)*sizeof(char));
         memset(temp_output_line, '\0', 1100);
 
         // Concatenate the intermediate output with the more global output which holds chars
-        // Then reset output_line
-        strcat(temp_output_line, output_line);
-        memset(output_line, '\0', 1100);
 
     	// Concat the temp output with the buffer
-    	strcat(temp_output_line, buffer3[buffer3_con_idx]);
-
-        // Checks to see if only DONE\n was written
-        int output_bool_cmp = strcmp(buffer3[buffer3_con_idx],"DONE\n");
-        if (output_bool_cmp == 0) {
-            output_bool = false;
+    	// strcat(temp_output_line, buffer3[buffer3_con_idx]);
+        prints(buffer3[buffer3_con_idx]);
+        strcat(output_line, buffer3[buffer3_con_idx]);
+        if (strlen(output_line) >= 80) {
+            int num_lines = strlen(output_line)/80;
+            for (int i = total_lines; i < num_lines; ++i) {
+                char line[81];
+                memset(line, '\0', sizeof(line));
+                for (int c = 0; c < 80; ++c) {
+                    line[c] = output_line[i*80+c];
+                }
+                // printf("%s\n", line);
+                printf("%03d: %s\n", i, line);
+            }
+            total_lines = num_lines;
+            fflush(NULL);
         }
 
         // Increment the consumer buffer count and decrement the main buffer count
@@ -456,12 +391,9 @@ void *b3_consumer(void *args) {
         // Unlock the mutex
         pthread_mutex_unlock(&b3_mutex);
 
-        // Only call write_output if output_bool is true
-        if (output_bool == true) {
-            
-            // Call write output on the temp output (has the buffer) and return any
-            // left over chars to be passed back in when the loop resets
-            strcpy(output_line, write_output(temp_output_line));
+        // printf("all done: %d, %d, %d, %d\n", all_done, buffer1_count, buffer2_count, buffer3_count);
+        if (all_done && buffer1_count == 0 && buffer2_count == 0 && buffer3_count == 0) {
+            exit(1);
         }
     	
     }
